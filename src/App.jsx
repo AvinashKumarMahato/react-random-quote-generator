ada4c95deb6d653f92082bc2f64addfe46b2c24f
Avinash kumar Mahato <akr34206@gmail.com>
2024-04-05 16:29:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import RandomQuote from "./RandomQuote/RandomQuote";
+
+function App() {
+
+  return (
+    <div>
+      <RandomQuote />
+    </div>
+  );
+}
+
+export default App;
